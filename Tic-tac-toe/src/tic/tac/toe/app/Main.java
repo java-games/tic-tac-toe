a64f28146be972a636a7609bfa1ec9c650dfae8c
Georgi Mirzoyan <mirzoyan87@mail.ru>
2016-03-12 17:42:06 +0400

    Added main window

---
+++ Tic-tac-toe/src/tic/tac/toe/app/Main.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package tic.tac.toe.app;
+
+
+/**
+ * @author georgi
+ * 
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		Window appWindow = new Window();
+		appWindow.setVisible(true);
+	}
+}
